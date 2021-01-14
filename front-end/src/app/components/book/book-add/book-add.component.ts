6db59251fd9ea379f1f4b33adee9a45e30e3554f
trandau2k2 <trantuyet.fightforfreedom@gmail.com>
2021-01-14 11:00:09 +0700

    add unread list books

--- front-end/src/app/components/book/book-add/book-add.component.ts
+++ front-end/src/app/components/book/book-add/book-add.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ValueConverter} from '@angular/compiler/src/render3/view/template';
+import {BookService} from '../../../services/book.service';
+import {HttpResult} from '../../../_core/HttpResult';
+import {Route, Router} from '@angular/router';
 
 @Component({
   selector: 'app-book-add',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookAddComponent implements OnInit {
 
-  constructor() { }
+  formAddBook: FormGroup;
+  constructor(private fb: FormBuilder,
+              private bookService: BookService,
+              private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.formAddBook = this.fb.group({
+      name: ['', [Validators.required]],
+      read: ['', [Validators.required]]
+    });
+  }
+
+  submit() {
+    // @ts-ignore
+    let data = this.formAddBook.value;
+    this.bookService.create(data).subscribe((res: HttpResult) => {
+      if (res.status == 'success') {
+        this.router.navigate(['admin/books']);
+      }
+    })
   }
 
 }
--- front-end/src/app/components/book/book-list-unread/book-list-unread.component.ts
+++ front-end/src/app/components/book/book-list-unread/book-list-unread.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BookService} from '../../../services/book.service';
+import {IBook} from '../../IBook';
+import {HttpResult} from '../../../_core/HttpResult';
 
 @Component({
   selector: 'app-book-list-unread',
   templateUrl: './book-list-unread.component.html',
-  styleUrls: ['./book-list-unread.component.css']
+  styleUrls: ['./book-list.component-unread.css']
 })
 export class BookListUnreadComponent implements OnInit {
 
-  constructor() { }
+  books: IBook[] | undefined;
+  message: string | undefined;
+
+  constructor(private bookService: BookService) {
+  }
 
   ngOnInit(): void {
+    this.getAllBook();
+  }
+
+  getAllBook(): void {
+    this.bookService.getAll().subscribe((res: HttpResult) => {
+      if (res.status == 'success') {
+        console.log('get all books unread successful!');
+        this.books = res.data;
+      } else {
+        console.log('cannot get any books!');
+      }
+    });
+  }
+
+  delete(id: number) {
+    if (confirm('Are you sure to delete this book?')) {
+      this.bookService.destroy(id).subscribe((res: HttpResult) => {
+        if (res.status == 'success') {
+          this.getAllBook();
+          this.message = res.message;
+        }
+      });
+    }
   }
 
 }
